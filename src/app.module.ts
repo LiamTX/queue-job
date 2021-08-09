825b678f04df424eeedca1ccf50e575923c18868
Liam Teixeira <liamcabral88@gmail.com>
2021-08-09 19:09:30 -0300

    mailer config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,31 @@
+import { MailerModule } from '@nestjs-modules/mailer';
+import { BullModule } from '@nestjs/bull';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CreateUserController } from './create-user/create-user.controller';
 
 @Module({
-  imports: [],
+  imports: [
+    BullModule.forRoot({
+      redis: {
+        host: 'localhost',
+        port: 6379
+      }
+    }),
+
+    MailerModule.forRoot({
+      transport: {
+        host: process.env.MAILER_HOST,
+        port: parseInt(process.env.MAILER_PORT),
+        auth: {
+          user: process.env.MAILER_USER,
+          pass: process.env.MAILER_PASS
+        }
+      }
+    })
+  ],
   controllers: [AppController, CreateUserController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
